ddab1954adf562d397997561c12ea7f75841dec8
Shrey Banga <shrey@quip.com>
2017-04-23 19:35:48 -0700

    Allow saving and loading app state

--- static/script.js
+++ static/script.js
@@ -3,6 +3,10 @@ const $visitor = $("#visitor");
 const $tree = $("#tree");
 const $output = $("#output");
 const $arrow = $("<div>➡</div>").addClass("error-arrow");
+const $save = $("#save");
+const $load = $("#load");
+const $stateName = $("#stateName");
+const $stateList = $("#stateList");
 var sourceEditor;
 var visitorEditor;
 var outputEditor;
@@ -55,7 +59,7 @@ function highlightLineNodes(lineno) {
 
 function updateHighlightedLineNodes() {
     const cursor = sourceEditor.getCursor();
-    highlightLineNodes(cursor.line);
+    highlightLineNodes(cursor.line + 1);
 }
 
 function makeEditor(value, node) {
@@ -94,11 +98,12 @@ function apiCall(endpoint) {
 const parseApi = apiCall("/parse");
 const transformApi = apiCall("/transform");
 const storage = localStorage;
+const kDefaultState = "default";
 
 function updateTree() {
-    storage.source = sourceEditor.getValue();
+    const state = saveState(kDefaultState);
     updateOutput();
-    parseApi({source: storage.source},
+    parseApi({source: state.source},
         function(data) {
             console.log(data.tree);
             $tree.html("").append(renderNode(data.tree));
@@ -108,10 +113,9 @@ function updateTree() {
 
 function updateOutput() {
     const gutterId = "CodeMirror-linenumbers";
-
-    storage.visitor = visitorEditor.getValue();
+    const state = saveState(kDefaultState);
     transformApi(
-        {source: storage.source, visitor: storage.visitor},
+        {source: state.source, visitor: state.visitor},
         function(data) {
             console.log(data);
             visitorEditor.getDoc().clearGutter(gutterId);
@@ -126,15 +130,69 @@ function updateOutput() {
         });
 }
 
+function stateName(name) {
+    return "state-" + name;
+}
+
+function saveState(name) {
+    var source = sourceEditor.getValue();
+    var visitor = visitorEditor.getValue();
+    var state = {
+        source: source,
+        visitor: visitor
+    };
+    storage[stateName(name)] = JSON.stringify(state);
+    updateStateList();
+    return state;
+}
+
+function getState(name) {
+    if (!(stateName(name) in storage)) {
+        return;
+    }
+    return JSON.parse(storage[stateName(name)]);
+}
+
+function loadState(name) {
+    var state = getState(name);
+    if (!state) {
+        console.warn(`State ${name} not found`);
+        return;
+    }
+    sourceEditor.setValue(state.source);
+    visitorEditor.setValue(state.visitor);
+}
+
+function updateStateList() {
+    $stateList.html("");
+    for (var key in storage) {
+        if (key.startsWith("state-")) {
+            var name = key.split("-")[1];
+            $stateList.append($("<option>").val(name).html(name));
+        }
+    }
+}
+
 function initialize() {
-    sourceEditor = makeEditor(storage.source, $source);
+    updateStateList();
+    sourceEditor = makeEditor("", $source);
     sourceEditor.on("cursorActivity", updateHighlightedLineNodes);
     sourceEditor.on("changes", updateTree);
 
-    visitorEditor = makeEditor(storage.visitor, $visitor);
+    visitorEditor = makeEditor("", $visitor);
     visitorEditor.on("changes", updateOutput);
 
-    updateTree(sourceEditor);
+    $save.click(function() {
+        const name = $stateName.val() || kDefaultState;
+        saveState(name);
+    });
+
+    $load.click(function() {
+        const name = $stateName.val() || kDefaultState;
+        loadState(name);
+    });
+
+    loadState(kDefaultState);
 }
 
 initialize();
